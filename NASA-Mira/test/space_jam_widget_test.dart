e190624681db64257156cd2cc9686bf9fcc4b282
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2022-01-09 22:01:11 +0100

    test for custom appbar

--- NASA-Mira/test/space_jam_widget_test.dart
+++ NASA-Mira/test/space_jam_widget_test.dart
@@ -12,19 +12,68 @@
 // - IconStyle
 
 // Flutter
-// import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:nasamira/widgets/appbar.dart";
 import "package:flutter_test/flutter_test.dart";
-
-// MiraApp
-// import 'package:nasamira/main.dart';
+import "package:nasamira/widgets/collection.dart";
 
 // widgets
 
 void main() {
   group("SpaceJam Widgets", () {
     // PageBuilder
-    // AppBar
+    // TODO(KristofKekesi): Implement it after sliver in Appbar.
+
+    // Appbar
+    testWidgets("AppBar", (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Appbar(
+            title: "title",
+            subtitle: "subtitle",
+            leftAction: AppBarAction(icon: Icons.add),
+            rightAction: AppBarAction(icon: Icons.remove),
+          ),
+        ),
+      );
+
+      // Search elements
+      final Finder titleFinder = find.text("title");
+      final Finder subtitleFinder = find.text("subtitle");
+      final Finder leftIconFinder = find.byIcon(Icons.add);
+      final Finder rightIconFinder = find.byIcon(Icons.remove);
+
+      // Verify
+      expect(titleFinder, findsOneWidget);
+      expect(subtitleFinder, findsOneWidget);
+      expect(leftIconFinder, findsOneWidget);
+      expect(rightIconFinder, findsOneWidget);
+    });
+
     // Collection
+    testWidgets("Collection", (WidgetTester tester) async {
+      // Visible
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Collection(
+            inputType: "type",
+            filter: "orbiter",
+            outputType: "type",
+            isVisible: ValueNotifier<bool>(true),
+          ),
+        ),
+      );
+
+      // Search elements
+      final Finder titleFinder = find.text("title");
+      final Finder subtitleFinder = find.text("subtitle");
+      final Finder iconFinder = find.byIcon(Icons.arrow_forward_rounded);
+
+      // Verify
+      expect(titleFinder, findsOneWidget);
+      expect(subtitleFinder, findsOneWidget);
+      expect(iconFinder, findsOneWidget);
+    });
     // CollectionItem
     // ContentBox
     // Button
